05a7665ba9c34e1e1b03c216f6c73d96d786993f
Alexandra <kguerrero@fp.lasalle.es>
2026-03-20 00:36:52 +0100

    guardar y listar gastos

--- src/HelloConsole.java
+++ src/HelloConsole.java
@@ -19,75 +19,113 @@ public class HelloConsole {
             opcion = sc.nextInt();
             sc.nextLine();
 
+            if (opcion == 1) {
+                do {
+                    System.out.println("----- MENU GASTOS -----");
+                    System.out.println("1. Anadir gasto");
+                    System.out.println("2. Editar gasto");
+                    System.out.println("3. Eliminar gasto");
+                    System.out.println("4. Listar gastos");
+                    System.out.println("0. Volver");
+                    System.out.print("Elige una opcion: ");
 
+                    opcionGastos = sc.nextInt();
+                    sc.nextLine();
+                    if (opcionGastos == 1) {
+                        System.out.println("Añadir gasto");
 
-                if (opcion == 1) {
-                    do {
-                        System.out.println("----- MENU GASTOS -----");
-                        System.out.println("1. Anadir gasto");
-                        System.out.println("2. Editar gasto");
-                        System.out.println("3. Eliminar gasto");
-                        System.out.println("4. Listar gastos");
-                        System.out.println("0. Volver");
-                        System.out.print("Elige una opcion: ");
+                        System.out.println("Introduce descripcion:");
+                        String descripcion = sc.nextLine();
 
-                        opcionGastos = sc.nextInt();
+                        System.out.println("Introduce importe:");
+                        double importe = sc.nextDouble();
                         sc.nextLine();
-                        if (opcionGastos == 1) {
-                            System.out.println("Añadir gasto");
 
-                            System.out.println("Introduce descripcion:");
-                            String descripcion = sc.nextLine();
+                        System.out.println("Introduce categoria:");
+                        String categoria = sc.nextLine();
+                        System.out.println("Introduce fecha:");
+                        String fecha = sc.nextLine();
+                        System.out.println("Introduce metodo de pago:");
+                        String metodoPago = sc.nextLine();
 
-                            System.out.println("Introduce importe:");
-                            double importe = sc.nextDouble();
-                            sc.nextLine();
-                            System.out.println("Introduce categoria:");
-                            String categoria = sc.nextLine();
-                            System.out.println("Introduce fecha:");
-                            String fecha = sc.nextLine();
-                            System.out.println("Introduce metodo de pago:");
-                            String metodoPago = sc.nextLine();
+                        Gasto g = new Gasto();
+                        g.descripcion = descripcion;
+                        g.importe = importe;
+                        g.categoria = categoria;
+                        g.fecha = fecha;
+                        g.metodoPago = metodoPago;
 
-                            Gasto g = new Gasto();
-                            g.descripcion = descripcion;
-                            g.importe = importe;
-                            g.categoria = categoria;
-                            g.fecha = fecha;
-                            g.metodoPago = metodoPago;
+                        gastos[numGastos] = g;
+                        numGastos++;
 
-                            gastos[numGastos] = g;
-                            numGastos++;
+                        System.out.println("Gasto guardado correctamente");
+                        System.out.println("Gasto creado correctamente");
 
-                            System.out.println("Gasto guardado correctamente");
+                        System.out.println("Has escrito: " + descripcion);
+                        System.out.println("El importe es: " + importe);
+                        System.out.println("Categoria: " + categoria);
+                        System.out.println("Fecha: " + fecha);
+                        System.out.println("Metodo de Pago:" + metodoPago);
+                    }
 
-                            System.out.println("Gasto creado correctamente");
+                    if (opcionGastos == 4) {
+                        System.out.println("LISTA DE GASTOS");
 
-                            System.out.println("Has escrito: " + descripcion);
-                            System.out.println("El importe es: " + importe);
-                            System.out.println("Categoria: " + categoria);
-                            System.out.println("Fecha: " + fecha);
-                            System.out.println("Metodo de Pago:" + metodoPago);
+                        for (int i = 0; i < numGastos; i++) {
+                            System.out.println(
+                                    gastos[i].descripcion + " - " +
+                                            gastos[i].importe + " - " +
+                                            gastos[i].categoria + " - " +
+                                            gastos[i].fecha + " - " +
+                                            gastos[i].metodoPago);
+                        }
+                    }
 
-                                }
-                        if (opcionGastos == 4) {
-                            System.out.println("LISTA DE GASTOS");
+                    if (opcionGastos == 2) {
+                        System.out.println("EDITAR GASTO");
 
-                            for (int i = 0; i < numGastos; i++) {
-                                System.out.println(
-                                        gastos[i].descripcion + " - " +
-                                                gastos[i].importe + " - " +
-                                                gastos[i].categoria + " - " +
-                                                gastos[i].fecha + " - " +
-                                                gastos[i].metodoPago   );
-                            }
+                        for (int i = 0; i < numGastos; i++) {
+                            System.out.println(i + " - " +
+                                    gastos[i].descripcion + " - " +
+                                    gastos[i].importe);
                         }
 
-                    } while (opcionGastos != 0);
-                }
+                        System.out.println("Que gasto quieres editar?");
+                        int posicion = sc.nextInt();
+                        sc.nextLine();
 
+                        System.out.println("Introduce el nuevo importe:");
+                        double nuevoImporte = sc.nextDouble();
+                        sc.nextLine();
+
+                        gastos[posicion].importe = nuevoImporte;
+
+                        System.out.println("Importe editado correctamente");
+                    }
+
+                    if (opcionGastos == 3) {
+                        System.out.println("ELIMINAR GASTO");
+
+                        for (int i = 0; i < numGastos; i++) {
+                            System.out.println(i + " - " +
+                                    gastos[i].descripcion + " - " +
+                                    gastos[i].importe);
+                        }
+
+                        System.out.println("Que gasto quieres eliminar?");
+                        int posicionEliminar = sc.nextInt();
+                        sc.nextLine();
+
+                        for (int i = posicionEliminar; i < numGastos - 1; i++) {
+                            gastos[i] = gastos[i + 1];
+                        }
+                        numGastos--;
+
+                        System.out.println("Gasto eliminado correctamente");
+                    }
+
+                } while (opcionGastos != 0);
             }
-            while (opcion != 0) ;
-        }
-
+        } while (opcion != 0);
+    }
 }
