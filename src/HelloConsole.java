7094d1f4da51457e64b15e1741ad6177f8c7334a
Alexandra <kguerrero@fp.lasalle.es>
2026-03-19 20:06:09 +0100

    creando bucles

--- src/HelloConsole.java
+++ src/HelloConsole.java
@@ -4,6 +4,7 @@ public class HelloConsole {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int opcion;
+        int opcionGastos = 0;
 
         do {
             System.out.println("----- MENU PRINCIPAL -----");
@@ -15,6 +16,25 @@ public class HelloConsole {
 
             opcion = sc.nextInt();
             sc.nextLine();
+
+            if (opcion == 1) {
+                do {
+                    System.out.println("----- MENU GASTOS -----");
+                    System.out.println("1. Anadir gasto");
+                    System.out.println("2. Editar gasto");
+                    System.out.println("3. Eliminar gasto");
+                    System.out.println("4. Listar gastos");
+                    System.out.println("0. Volver");
+                    System.out.print("Elige una opcion: ");
+
+                    opcionGastos = sc.nextInt();
+                    sc.nextLine();
+                    if (opcionGastos == 1) {
+                        System.out.println("Añadir gasto");
+                    }
+
+                } while (opcionGastos != 0);
+            }
 
         } while (opcion != 0);
     }
